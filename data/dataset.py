8e2441a4cdd7bec00e25e25316014ee54fddc180
Vinh2608 <42367441+Vinh2608@users.noreply.github.com>
2023-07-10 12:43:49 +0700

    Update dataset.py chane the mean and std

--- data/dataset.py
+++ data/dataset.py
@@ -24,7 +24,8 @@ class Dataset(data.Dataset):
         # normalize = T.Normalize(mean=[0.5, 0.5, 0.5],
         #                         std=[0.5, 0.5, 0.5])
 
-        normalize = T.Normalize(mean=[0.5], std=[0.5])
+        normalize = T.Normalize(mean = [0.60746885, 0.47471561, 0.41313071]
+                                  std = [0.2621818,0.23118107,0.2242216])
 
         if self.phase == 'train':
             self.transforms = T.Compose([
